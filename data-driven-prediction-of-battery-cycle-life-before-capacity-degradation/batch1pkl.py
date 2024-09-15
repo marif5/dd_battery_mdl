a23e907f0d4d871c77b010b8e4bfd9e29c6dc97d
Adeel Arif <42553246+marif5@users.noreply.github.com>
2024-09-15 17:09:36 -0400

    comit

---
+++ data-driven-prediction-of-battery-cycle-life-before-capacity-degradation/batch1pkl.py
@@ -0,0 +1,14 @@
+import h5py
+import scipy.io
+import matplotlib.pyplot as plt
+import numpy as np
+import pickle
+
+matFilename = './Data/2017-05-12_batchdata_updated_struct_errorcorrect.mat'
+f = h5py.File(matFilename)
+
+list(f.keys())
+
+batch = f['batch']
+
+list(batch.keys())
