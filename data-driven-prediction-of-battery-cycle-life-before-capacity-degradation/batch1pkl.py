b78649a92204994c8c69ab150148a5ab426ec96b
Mish Wilson <mwilso33@uvm.edu>
2024-10-23 22:00:19 -0400

    modified code

--- data-driven-prediction-of-battery-cycle-life-before-capacity-degradation/batch1pkl.py
+++ data-driven-prediction-of-battery-cycle-life-before-capacity-degradation/batch1pkl.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pickle
 
-matFilename = './Data/2017-05-12_batchdata_updated_struct_errorcorrect.mat'
+matFilename = './Data/2017-06-30_batchdata_updated_struct_errorcorrect.mat'
 f = h5py.File(matFilename)
 
 list(f.keys())
